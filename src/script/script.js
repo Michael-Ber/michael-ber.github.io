64ed00726ef9f67fb5776341eb59c9b1c1039598
Michael <example@gmail.com>
2020-07-08 15:08:39 +0800

    end adaptation

--- src/script/script.js
+++ src/script/script.js
@@ -1,20 +1,31 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
-          short_menu = document.querySelector('.short_menu');
+          hamburger = document.querySelector('.hamburger');
 
-    short_menu.addEventListener('click', () => {
-        if(menu.classList.contains('activity')) {
-            menu.classList.remove('activity');
-            menu.style.left = '-50%';
-        }else {
-            menu.classList.add('activity');
-            menu.style.left = 0
-        }
+    hamburger.addEventListener('click', () => {
+        menu.classList.toggle('menu_active');
+        hamburger.classList.toggle('hamburger_active');
 
-        if(short_menu.classList.contains('activity')) {
-            short_menu.classList.remove('activity');
-        }else {
-            short_menu.classList.add('activity')
-        }
-    })
-})
+    });
+});
+
+
+
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     const menu = document.querySelector('.menu'),
+//     menuItem = document.querySelectorAll('.menu_item'),
+//     hamburger = document.querySelector('.hamburger');
+
+//     hamburger.addEventListener('click', () => {
+//         hamburger.classList.toggle('hamburger_active');
+//         menu.classList.toggle('menu_active');
+//     });
+
+//     menuItem.forEach(item => {
+//         item.addEventListener('click', () => {
+//             hamburger.classList.toggle('hamburger_active');
+//             menu.classList.toggle('menu_active');
+//         })
+//     })
+// })
